c88af84d25f6d8328923c619b8b04b5a8c211f7e
Benjamin Vg <vgbenjamin@gmail.com>
2015-09-07 09:35:35 +0200

    Rules Field

---
+++ Fortis/Helpers/RulesHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Rules;
+
+namespace Fortis.Helpers
+{
+    public static class RulesHelper
+    {
+        public static bool EvaluateRule(Field field)
+        {
+            var ruleContext = new RuleContext();
+
+            foreach (Rule<RuleContext> rule in RuleFactory.GetRules<RuleContext>(field).Rules)
+            {
+                if (!EvaluateSingleRule(rule, ruleContext))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool EvaluateSingleRule(Rule<RuleContext> rule, RuleContext ruleContext)
+        {
+            if (rule.Condition == null)
+                return true;
+
+            var stack = new RuleStack();
+            rule.Condition.Evaluate(ruleContext, stack);
+
+            if (ruleContext.IsAborted)
+            {
+                return false;
+            }
+            return (stack.Count != 0) && ((bool)stack.Pop());
+        }
+    }
+}
---
+++ Fortis/Model/Fields/RulesFieldWrapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Fortis.Helpers;
+using Sitecore.Data.Fields;
+using Fortis.Providers;
+
+namespace Fortis.Model.Fields
+{
+	public class RulesFieldWrapper : FieldWrapper, IRulesFieldWrapper
+    {
+		public string Value => _rawValue;
+
+	    public RulesFieldWrapper(Field field, ISpawnProvider spawnProvider)
+			: base(field, spawnProvider) { }
+
+		public RulesFieldWrapper(string key, ref ItemWrapper item, ISpawnProvider spawnProvider, string value = null)
+			: base(key, ref item, value, spawnProvider)
+		{
+		}
+
+	    public bool Evaluate()
+	    {
+	        return RulesHelper.EvaluateRule(this.Field);
+	    }
+	}
+}
